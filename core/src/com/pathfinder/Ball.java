6fbed00dc6a2635064fac5ab7aa0c05a28265954
Parsa Salamatipour <sa.parsa20@gmail.com>
2023-06-01 21:34:28 +0330

    #RESOLUTION_CHANGING

--- core/src/com/pathfinder/Ball.java
+++ core/src/com/pathfinder/Ball.java
@@ -1,13 +1,44 @@
 package com.pathfinder;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Ball
 {
+    float x = 10;
+    float y = 10;
+    float speed = 1;
+    float direction = 45;
+    float angularSpeed = 2;
+    float radius = 4;
+    float expansion = 0.1f;
+
+    int time = 0;
+
+    public Ball()
+    {
+
+    }
+
+    private void move()
+    {
+        if(time < 100)
+            radius += expansion;
+        else
+            direction -= angularSpeed;
+
+        x += speed * MathUtils.cosDeg(direction);
+        y += speed * MathUtils.sinDeg(direction);
+
+        time++;
+    }
+
     public void draw(ShapeRenderer shapeRenderer)
     {
-        shapeRenderer.begin();
-        shapeRenderer.circle(0, 0, 1);
-        shapeRenderer.end();
+        move();
+
+        shapeRenderer.setColor(1, 0, 0, 1);
+        shapeRenderer.circle(x, y, radius);
     }
 }
--- core/src/com/pathfinder/MyGdxGame.java
+++ core/src/com/pathfinder/MyGdxGame.java
@@ -2,48 +2,99 @@ package com.pathfinder;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ai.pfa.GraphPath;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import javax.swing.*;
-
 public class MyGdxGame extends ApplicationAdapter
 {
-	ShapeRenderer shapeRenderer;
+	ShapeRenderer shapeBatch;
 	SpriteBatch batch;
-	BitmapFont font;
-	Texture screen;
+	FrameBuffer frameBuffer;
 	Ball ball;
+	public OrthographicCamera fboCamera;
+
+	int screenWidth;
+	int screenHeight;
+	float aspectRatio;
+
+	int fboWidth=100;
+	int fboHeight=100;
+	int BINDING_DEPTH_FRAMEBUFFER=3;
+	int BINDING_DEFAULT=0;
+
+	void createFrameBuffer()
+	{
+		if (frameBuffer !=null)
+		{
+			frameBuffer.dispose();
+		}
+		frameBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, fboWidth, fboHeight, false);
+		frameBuffer.getColorBufferTexture().setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+		frameBuffer.getColorBufferTexture().bind(BINDING_DEPTH_FRAMEBUFFER);
+		Gdx.graphics.getGL20().glActiveTexture(GL20.GL_TEXTURE0 + BINDING_DEFAULT); //make sure this is set to 0 for batch draws
+		fboCamera=new OrthographicCamera();
+		fboCamera.setToOrtho(false, fboWidth, fboHeight);
+		fboCamera.update();
+	}
 
 	@Override
 	public void create()
 	{
-		shapeRenderer = new ShapeRenderer();
 		batch = new SpriteBatch();
-		font = new BitmapFont();
-
+		shapeBatch=new ShapeRenderer();
 		ball = new Ball();
+
+		screenWidth = Gdx.graphics.getWidth();
+		screenHeight = Gdx.graphics.getHeight();
+		aspectRatio = (float) screenWidth / screenHeight;
+
+		fboWidth *= aspectRatio;
 	}
 
 	@Override
 	public void render()
 	{
-		Gdx.gl.glClearColor(.5f, .5f, .5f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		int width = Gdx.graphics.getWidth();
+		int height = Gdx.graphics.getHeight();
 
-		ball.draw(shapeRenderer);
+//I had some initialisation problems with the fbo when trying to create a framebuffer. Need to be absolutely certain
+		//size is correct and opengl context is set up before creating the framebuffer (as far as I remember). This if statement and the
+		//createFrameBuffer function works for me and will allow you to change the size of fboWidth/fboHeight on the fly (though will cause stutters if you do)
+		if ((frameBuffer ==null) || (frameBuffer.getWidth()!=fboWidth) || (frameBuffer.getHeight()!=fboHeight))
+		{
+			createFrameBuffer();
+		}
+
+		//start drawing to a small framebuffer, that we will then scale up to create a pixelated effect
+		frameBuffer.begin();
+
+		ScreenUtils.clear(0, 0, 0, 0.5f);
+		shapeBatch.begin(ShapeRenderer.ShapeType.Line);
+		shapeBatch.setProjectionMatrix(fboCamera.combined);
+		Gdx.gl.glDisable(GL20.GL_BLEND);
+		ball.draw(shapeBatch);
+		shapeBatch.end();
+
+		frameBuffer.end();
+
+
+		ScreenUtils.clear(0, 0, 0, 1);
+		batch.begin();
+		batch.draw(frameBuffer.getColorBufferTexture(), 20, 20, width, height, 0, 0, 1, 1);
+		batch.end();
 	}
 
 	@Override
 	public void dispose()
 	{
-		shapeRenderer.dispose();
 		batch.dispose();
-		font.dispose();
+		shapeBatch.dispose();
+		frameBuffer.dispose();
 	}
 }
